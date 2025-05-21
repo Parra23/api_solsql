f9df2b8bc7fe458d9e295e3cb9ed2a571cf97520
jorluis <jorluis.parra@cecar.edu.co>
2025-05-20 23:05:28 -0500

    role_login2

--- api_solsql/Models/LoginRequest.cs
+++ api_solsql/Models/LoginRequest.cs
@@ -1,8 +1,9 @@
 namespace api_solsql.Models;
 
-public class LoginRequest 
+public class LoginRequest
 {
 	public int Id { get; set; }
 	public string Email { get; set; }
 	public string Password { get; set; }
+	public int Role { get; set; }
 }
