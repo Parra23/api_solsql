fbab9d2756433e4da358e7d10463877fe74e2515
jorluis <jorluis.parra@cecar.edu.co>
2025-05-24 13:45:08 -0500

    Se creó el controlador vw_logs

---
+++ api_solsql/Controllers/vw_logsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api_solsql.Context;
+using api_solsql.Models;
+namespace api_solsql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class vw_logsController : ControllerBase
+    {
+        private readonly Context.ContextDB _context;
+
+        public vw_logsController(Context.ContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/vw_logs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<vw_logs>>> GetLogs()
+        {
+            try
+            {
+                var logsList = await _context.vw_log
+                    .FromSqlInterpolated($"CALL sp_get_logs()")
+                    .ToListAsync();
+                return Ok(logsList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Unexpected error", error = ex.Message });
+            }
+        }
+    }
+}
